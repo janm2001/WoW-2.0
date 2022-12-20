a3e374e00dbe027c3deaa7f863580f79f2d61acd
Janm20 <jan.mackovic@datablast.eu>
2022-12-20 19:38:25 +0100

    Changes to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { Link } from '@mui/material'
 import './Navbar.css'
 import logo from '../../assets/wow-logo.jpg'
+import {AiOutlineMenu} from 'react-icons/ai'
+import { useState } from 'react'
+import {IconButton} from '@mui/material'
 
 
 
 const Navbar = () => {
+  const [menu,setMenu] = useState(false);
+
   return (
     <nav>
       <div className="wrap">
@@ -20,6 +25,15 @@ const Navbar = () => {
                 <Link href='#'>EXPANSIONS</Link>
                 <Link href='#'>CLASSES</Link>
                 <Link href='#'>NEWS</Link>
+            </div>
+
+            <div className='smallerDisplay'>
+              
+              <AiOutlineMenu />
+              
+
+              {/* {menu && <div>I'm here</div>} */}
+
             </div>
       </div>
            
